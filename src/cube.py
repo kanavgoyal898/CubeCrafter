64effb3dda02a7fb6885d143a99d3d2fb10b9aff
Kanav Goyal <kanavgoyal898@gmail.com>
2025-04-22 22:08:38 +0530

    Implement Vertical Rotation

--- src/cube.py
+++ src/cube.py
@@ -49,6 +49,10 @@ class Cube:
         # self.config[0] = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]  # Up
         # self.config[5] = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]  # Down
 
+        # vertical rotate testing
+        # self.config[1] = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]  # Left
+        # self.config[3] = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]  # Right
+
     def horizontal_rotate(self, row, direction):
         """
         Performs a horizontal rotation of the specified row across the four lateral faces.
@@ -87,13 +91,53 @@ class Cube:
                 # clockwise rotation for the bottom row
                 self.config[5] = ([list(row) for row in zip(*reversed(self.config[5]))])
 
+    def vertical_rotate(self, col, direction):
+        """
+        Performs a vertical rotation of the specified column across the four lateral faces.
+        If the column is the left or right, the Left or Right face is rotated accordingly.
+
+        Args:
+            col (int): The index of the column to rotate (0-based).
+            direction (str): Direction of rotation, either 'up' or 'down'.
+
+        Raises:
+            ValueError: If column is out of bounds or direction is invalid.
+        """
+
+        if col < 0 or col >= self.n:
+            raise ValueError("Column index out of bounds.")
+        if direction not in ['up', 'down']:
+            raise ValueError("Direction must be 'up' or 'down'.")
+        
+        face_1, face_2, face_3, face_4 = self.config[0], self.config[2], self.config[5], self.config[4]
+
+        if direction == 'up':
+            for i in range(self.n):
+                self.config[0][i][col], self.config[2][i][col], self.config[5][i][col], self.config[4][i][col] = face_2[i][col], face_3[i][col], face_4[i][col], face_1[i][col] 
+            if col == 0:
+                # counter-clockwise rotation for the left column
+                self.config[1] = [list(row) for row in zip(*self.config[1])][::-1]
+            if col == self.n - 1:
+                # clockwise rotation for the right column
+                self.config[3] = [list(row) for row in zip(*reversed(self.config[3]))]
+
+        if direction == 'down':
+            for i in range(self.n):
+                self.config[0][i][col], self.config[2][i][col], self.config[5][i][col], self.config[4][i][col] = face_4[i][col], face_1[i][col], face_2[i][col], face_3[i][col]
+            if col == 0:
+                # clockwise rotation for the left column
+                self.config[1] = [list(row) for row in zip(*reversed(self.config[1]))]
+            if col == self.n - 1:
+                # counter-clockwise rotation for the right column
+                self.config[3] = [list(row) for row in zip(*self.config[3])][::-1]
+
 cube = Cube()
 print(f"CUBE CONFIGURATION: \n{cube}")
 
-for row in range(cube.n):
-    for direction in ['left', 'right']:
-        cube.horizontal_rotate(row, direction)
+for col in range(cube.n):
+    for direction in ['up', 'down']:
+        cube.vertical_rotate(col, direction)
         print(f"===========================")
-        print(f"{direction.upper()} ROTATION ON ROW {row}: \n{cube}")
+        print(f"{direction.upper()} ROTATION ON COLUMN {col}: \n{cube}")
 
         cube.reset()
